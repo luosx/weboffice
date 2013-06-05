5834954b22487fdb021574dabd80c6ed4abffa2a
chenqiangfeng <chenqiangfeng@ee73b936-b0f0-8078-0053-b6be094e6ff3>
2013-06-05 03:38:47 +0000

    GPS工具设置

--- trunk/domain/src/com/klspta/base/util/api/IGPGGAUtil.java
+++ trunk/domain/src/com/klspta/base/util/api/IGPGGAUtil.java
@@ -5,6 +5,12 @@ package com.klspta.base.util.api;
 
 import com.klspta.base.wkt.Point;
 
+/**
+ * <br>Title:GPGG工具
+ * <br>Description:GPGGA信息处理
+ * <br>Author:陈强峰
+ * <br>Date:2013-6-5
+ */
 public interface IGPGGAUtil {
 	
 	void setGPGGA(String gpgga);
--- trunk/domain/src/com/klspta/base/util/api/IGetGPSPointUtil.java
+++ trunk/domain/src/com/klspta/base/util/api/IGetGPSPointUtil.java
@@ -1,5 +1,11 @@
 package com.klspta.base.util.api;
 
+/**
+ * <br>Title:GPS端口控制
+ * <br>Description:控制GPS开关和获取80点数据
+ * <br>Author:陈强峰
+ * <br>Date:2013-6-5
+ */
 public interface IGetGPSPointUtil {
     public String[] getXY();
     public boolean openCOM();
