56199b46254475dfc4f625822dd751ee4a77dfc3
chenqiangfeng <chenqiangfeng@ee73b936-b0f0-8078-0053-b6be094e6ff3>
2013-06-05 09:38:07 +0000

    压缩

--- src/com/klspta/base/util/api/IZIPUtil.java
+++ src/com/klspta/base/util/api/IZIPUtil.java
@@ -3,7 +3,6 @@
  */
 package com.klspta.base.util.api;
 
-import java.util.Vector;
 
 /**
  * <br>Title:压缩工具
@@ -12,23 +11,22 @@ import java.util.Vector;
  * <br>Date:2013-6-5
  */
 public interface IZIPUtil {
-     
-	/**
-	 * <br>Description:压缩
-	 * <br>Author:陈强峰
-	 * <br>Date:2013-6-5
-	 * @param zipFileName 
-	 * @param inputFile
-	 */
-	public void zip(String zipFileName,String inputFile);
 
-	   
-	/**
-	 * <br>Description:解压缩
-	 * <br>Author:陈强峰
-	 * <br>Date:2013-6-5
-	 * @param zipFileName
-	 * @param outputDirectory
-	 */
-	public void unZip(String zipFileName,String outputDirectory);
+    /**
+     * <br>Description:压缩
+     * <br>Author:陈强峰
+     * <br>Date:2013-6-5
+     * @param zipFileName  压缩最终完整路径
+     * @param inputFile    需要压缩的文件夹
+     */
+    public void zip(String zipFileName, String inputFile);
+
+    /**
+     * <br>Description:解压缩
+     * <br>Author:陈强峰
+     * <br>Date:2013-6-5
+     * @param zipFileName     压缩文件
+     * @param outputDirectory  解压缩位置
+     */
+    public void unZip(String zipFileName, String outputDirectory);
 }
