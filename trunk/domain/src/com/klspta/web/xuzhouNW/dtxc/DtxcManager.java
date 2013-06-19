309cf6e97574c504a644373cb12c77ce4773fa25
chenqiangfeng <chenqiangfeng@ee73b936-b0f0-8078-0053-b6be094e6ff3>
2013-06-19 09:00:03 +0000

    查询功能

--- trunk/domain/src/com/klspta/web/xuzhouNW/dtxc/DtxcManager.java
+++ trunk/domain/src/com/klspta/web/xuzhouNW/dtxc/DtxcManager.java
@@ -121,5 +121,18 @@ public class DtxcManager extends AbstractBaseBean {
 		String sql = "select * from xcrz t where t.yw_guid = ?";
 		return query(sql, YW, new Object[]{yw_guid});
 	}
-
+    /**
+     * 
+     * <br>Description:查询回传内容能
+     * <br>Author:陈强峰
+     * <br>Date:2013-4-9
+     * @param yw_guid
+     * @return
+     */
+    public Map<String, Object> getXckcqkData(String yw_guid) {
+        String sql = "select * from pad_xcxcqkb t where t.yw_guid=?";
+        List<Map<String, Object>> result = query(sql, YW,
+                new String[] { yw_guid });
+        return result.get(0);
+    }
 }
