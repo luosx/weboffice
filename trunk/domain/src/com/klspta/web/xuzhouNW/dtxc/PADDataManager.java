c888b35b4874b02e1528a74dae7496476f0c6fe6
chenqiangfeng <chenqiangfeng@ee73b936-b0f0-8078-0053-b6be094e6ff3>
2013-06-21 05:46:48 +0000

    行政区模糊

--- trunk/domain/src/com/klspta/web/xuzhouNW/dtxc/PADDataManager.java
+++ trunk/domain/src/com/klspta/web/xuzhouNW/dtxc/PADDataManager.java
@@ -30,7 +30,7 @@ public class PADDataManager extends AbstractBaseBean {
         String sql = "select t.readflag,t.guid,t.xzqmc,t.xmmc,t.rwlx,t.sfwf,(select u.fullname from core.core_users u where u.username=t.xcr) xcr,t.xcrq,t.cjzb,t.jwzb,t.imgname from v_pad_data t";
         if (keyword != null) {
             keyword = UtilFactory.getStrUtil().unescape(keyword);
-            sql = "select t.readflag,t.guid,t.xzqmc,t.xmmc,t.rwlx,t.sfwf,(select u.fullname from core.core_users u where u.username=t.xcr) xcr,t.xcrq,t.cjzb,t.jwzb,t.imgname from v_pad_data t where (upper(guid)||upper(xmmc)||upper(rwlx)||upper(sfwf)||upper(xcr)||upper(xcrq) like '%"
+            sql = "select t.readflag,t.guid,t.xzqmc,t.xmmc,t.rwlx,t.sfwf,(select u.fullname from core.core_users u where u.username=t.xcr) xcr,t.xcrq,t.cjzb,t.jwzb,t.imgname from v_pad_data t where (upper(guid)||upper(xzqmc)||upper(xmmc)||upper(rwlx)||upper(sfwf)||upper(xcr)||upper(xcrq) like '%"
                     + keyword + "%')";
         }
         List<Map<String, Object>> query = query(sql, YW);
