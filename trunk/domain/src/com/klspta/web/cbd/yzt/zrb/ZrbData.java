78549084fa9948511e78e14f45f5db7c64159643
chenqiangfeng <chenqiangfeng@ee73b936-b0f0-8078-0053-b6be094e6ff3>
2013-10-29 01:51:17 +0000

    缓存

--- trunk/domain/src/com/klspta/web/cbd/yzt/zrb/ZrbData.java
+++ trunk/domain/src/com/klspta/web/cbd/yzt/zrb/ZrbData.java
@@ -10,50 +10,62 @@ import com.klspta.base.util.UtilFactory;
 import com.klspta.web.cbd.yzt.utilList.IData;
 
 public class ZrbData extends AbstractBaseBean implements IData {
-	private static final String formName = "JC_ZIRAN";
-	/**
-	 * 
-	 * <br>Description:获取所有自然斑列表
-	 * <br>Author:黎春行
-	 * <br>Date:2013-10-18
-	 * @param request
-	 */
-	public List<Map<String, Object>> getAllList(HttpServletRequest request){
-		String sql = "select * from " + formName + " t order by to_number(t.yw_guid)";
-		return query(sql, YW);
-	}
-	
-	/**
-	 * 
-	 * <br>Description:查询自然斑
-	 * <br>Author:黎春行
-	 * <br>Date:2013-10-21
-	 * @param request
-	 * @return
-	 */
-	@Override
-	public List<Map<String, Object>> getQuery(HttpServletRequest request){
-		String keyWord = request.getParameter("keyWord");
-		StringBuffer querySql = new StringBuffer();
-		querySql.append("select * from ").append(formName).append(" t ");
-		if(keyWord != null){
-			keyWord = UtilFactory.getStrUtil().unescape(keyWord);
-			querySql.append("where t.yw_guid||t.zrbbh||t.zdmj||t.lzmj||t.cqgm||t.zzlzmj||t.zzcqgm||t.yjhs||t.fzzlzmj||t.fzzcqgm||t.bz like '%");
-			querySql.append(keyWord).append("%'");
-		}
-		querySql.append(" order by to_number(t.yw_guid)");
-		return query(querySql.toString(), YW);
-	}
-	
-	/**
-	 * 
-	 * <br>Description:更新自然斑数据
-	 * <br>Author:黎春行
-	 * <br>Date:2013-10-21
-	 * @param request
-	 * @return
-	 */
-	public String updateZrb(HttpServletRequest request){
-		return null;
-	}
+    private static final String formName = "JC_ZIRAN";
+
+    /**
+     * 保存自然斑列表
+     */
+    private static List<Map<String, Object>> zrbList;
+
+    /**
+     * 
+     * <br>Description:获取所有自然斑列表
+     * <br>Author:黎春行
+     * <br>Date:2013-10-18
+     * @param request
+     */
+    public List<Map<String, Object>> getAllList(HttpServletRequest request) {
+        if (zrbList != null) {
+            return zrbList;
+        }
+        String sql = "select * from " + formName + " t order by to_number(t.yw_guid)";
+        return query(sql, YW);
+    }
+
+    /**
+     * 
+     * <br>Description:查询自然斑
+     * <br>Author:黎春行
+     * <br>Date:2013-10-21
+     * @param request
+     * @return
+     */
+    @Override
+    public List<Map<String, Object>> getQuery(HttpServletRequest request) {
+        String keyWord = request.getParameter("keyWord");
+        StringBuffer querySql = new StringBuffer();
+        querySql.append("select * from ").append(formName).append(" t ");
+        if (keyWord != null) {
+            keyWord = UtilFactory.getStrUtil().unescape(keyWord);
+            querySql
+                    .append("where t.yw_guid||t.zrbbh||t.zdmj||t.lzmj||t.cqgm||t.zzlzmj||t.zzcqgm||t.yjhs||t.fzzlzmj||t.fzzcqgm||t.bz like '%");
+            querySql.append(keyWord).append("%'");
+        }else if(zrbList!=null){
+            return zrbList;
+        }
+        querySql.append(" order by to_number(t.yw_guid)");
+        return query(querySql.toString(), YW);
+    }
+
+    /**
+     * 
+     * <br>Description:更新自然斑数据
+     * <br>Author:黎春行
+     * <br>Date:2013-10-21
+     * @param request
+     * @return
+     */
+    public String updateZrb(HttpServletRequest request) {
+        return null;
+    }
 }
