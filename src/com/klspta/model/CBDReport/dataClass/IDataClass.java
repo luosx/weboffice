954b53ecca13b4a51df144ecf5e20af9fdbf123c
chenqiangfeng <chenqiangfeng@ee73b936-b0f0-8078-0053-b6be094e6ff3>
2013-10-31 07:00:24 +0000

    添加参数 便于行合并

--- src/com/klspta/model/CBDReport/dataClass/IDataClass.java
+++ src/com/klspta/model/CBDReport/dataClass/IDataClass.java
@@ -5,5 +5,5 @@ import java.util.Map;
 import com.klspta.model.CBDReport.bean.TRBean;
 
 public interface IDataClass {
-    Map<String, TRBean> getTRBeans(Object[] obj);
+    Map<String, TRBean> getTRBeans(Object[] obj,TRBean trBean);
 }
